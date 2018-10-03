54a4cc21fea4bf524d231ee100a18f7c4ef2f5eb
Hlumisa Mazomba <hlumzo@gmail.com>
2018-10-03 20:57:56 +0200

    Vizwall 1.0.0
    added Image Recognition using Clarifai and Expo

---
+++ Vizwall/.eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+    "extends": "airbnb", 
+    'parser': 'babel-eslint',
+    'env': {
+        jest: true,
+    },
+    'rules': {
+        'no-use-before-define': 'off',
+        'react/jsx-filename-extension': 'off',
+        'react/prop-types': 'off',
+        'global-require': 'off', 
+        'lines-between-class-members': 'off',
+        'object-curly-spacing': 'off',
+        'key-spacing': 'off',
+        'comma-dangle': 'off',
+        'space-before-function-paren': 'off',
+        'object-curly-newline' : 'off',
+        'keyword-spacing': 'off',
+        'arrow-spacing' : 'off',
+        'no-trailing-spaces': 'off',
+        'space-before-blocks': 'off',
+        'no-else-return': 'off',
+    },
+};
--- Vizwall/App.js
+++ Vizwall/App.js
@@ -1,21 +1,132 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View, TouchableOpacity, FlatList } from 'react-native';
+import { Camera, Permissions, ImageManipulator } from 'expo';
 
-export default class App extends React.Component {
-  render() {
-    return (
-      <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-      </View>
+const Clarifai = require('clarifai');
+
+
+const clarifai = new Clarifai.App({
+  apiKey: '094f38a8ca0247f790197b107c107554',
+});
+process.nextTick = setImmediate;
+export default class CameraExample extends React.Component {
+  state = {
+    hasCameraPermission: null,
+    predictions: [],
+    type: '',
+  };
+
+  async componentWillMount() {
+    const { status } = await Permissions.askAsync(Permissions.CAMERA);
+    this.setState({ hasCameraPermission: status === 'granted' });
+  }
+
+  capturePhoto = async () => {
+    if(this.camera) {
+      let photo = await this.camera.takePictureAsync();
+      return photo.uri;
+    }
+  };
+  resize = async photo => {
+    let manipulatedImage = await ImageManipulator.manipulate(
+      photo,
+      [{resize: {height: 300, width: 300}}],
+      { base64 : true}
     );
+    return manipulatedImage.base64
+  };
+  predict = async image => {
+    let predictions = await clarifai.models.predict(
+      Clarifai.GENERAL_MODEL, 
+      image
+    );
+    return predictions;
+  };
+  objectDetection = async () => {
+    let photo = await  this.capturePhoto();
+    let resized = await  this.resize(photo);
+    let predictions = await this.predict(resized);
+    this.setState({predictions: predictions.outputs[0].data.concepts});
+  };
+  render() {
+    const { hasCameraPermission, predictions } = this.state;
+    if (hasCameraPermission === null) {
+      return <View />;
+    } else if (hasCameraPermission === false) {
+      return <Text>No access to camera</Text>;
+    } else {
+      return (
+        <View style={{ flex: 1 }}>
+          <Camera 
+            ref={ref => {
+              this.camera = ref;
+            }}
+            style={{ flex: 1 }}
+            type={this.state.type}
+          >
+            <View
+              style={{    
+                flex: 1,
+                backgroundColor: 'transparent',
+                flexDirection: 'column',
+                justifyContent: 'flex-end'
+              }}
+            >
+              <View
+                style={{
+                  flex:1,
+                  alignSelf: 'flex-start',
+                  alignItems: 'center',
+                }}
+              >
+                <FlatList
+                  data={predictions.map(prediction => ({
+                    key : `${prediction.name} ${prediction.value}`,
+                  }))} 
+                  renderItem={({item}) => (
+                    <Text style={{paddingLeft: 15, color: 'white', fontSize: 20}}>{item.key}</Text>
+                  )}  
+                /> 
+
+              </View>
+              <TouchableOpacity
+                style={{
+                  flex: 0.1,
+                  alignItems: 'center',
+                  backgroundColor: 'blue', 
+                  height: '10%',
+                }}
+                onPress={this.objectDetection}
+              > 
+                <Text style={{fontSize: 30, color: 'white', padding: 15}}>
+                  {' '}
+                  Detect Objects{' '}  
+                </Text>
+
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={{
+                  flex: 0.1,
+                  alignSelf: 'flex-end',
+                  alignItems: 'center',
+                }}
+                onPress={() => {
+                  this.setState({
+                    type: this.state.type === Camera.Constants.Type.back
+                      ? Camera.Constants.Type.front
+                      : Camera.Constants.Type.back,
+                  });
+                }}                
+              >
+                <Text
+                  style={{ fontSize: 18, marginBottom: 10, color: 'white' }}>
+                  {' '}Flip{' '}
+                </Text>
+              </TouchableOpacity>
+            </View>
+          </Camera>
+        </View>
+      );
+    }
   }
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
